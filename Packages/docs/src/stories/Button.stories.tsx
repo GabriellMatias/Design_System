19e3eb2b58ac2dfb8ee83fefbbe63a4ae980a074
Dev.Matias <gabriel.goiasat@gmail.com>
2023-01-24 19:10:26 -0300

    :hammer: Build: Finished storybook tokens and adding turbo dependecie

---
+++ Packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,16 @@
+import { Button, ButtonProps } from '@ignite-ui/react'
+import { StoryObj, Meta } from '@storybook/react'
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'enviar',
+  },
+} as Meta<ButtonProps>
+export const Primary: StoryObj<ButtonProps> = {}
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big',
+  },
+}
---
+++ Packages/react/src/index.tsx
@@ -0,0 +1,27 @@
+import { ComponentProps } from 'react'
+import { styled } from './styles'
+
+export const Button = styled('button', {
+  fontFamily: '$default',
+  backgroundColor: '$ignite300',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  borderRadius: '$sm',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: { fontSize: 16, padding: '$3 $6' },
+    },
+  },
+  defaultVariants: {
+    size: 'small',
+  },
+})
+
+export type ButtonProps = ComponentProps<typeof Button>
